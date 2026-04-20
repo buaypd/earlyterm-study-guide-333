70d8739e982a9b835c4f3109ed0e05ffe445eda6
Buay Dak <buaypd@gmail.com>
2026-04-19 23:06:15 -0700

    largestMinusSmallest test

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,5 +1,8 @@
 import static org.junit.Assert.*;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 public class PracticeTest {
@@ -20,7 +23,22 @@ public class PracticeTest {
     
 
     // TODO: Make tests for each problem you solve
-    
+    @Test
+    void test() {
+        // Arrange
+        Set<Integer> testSet = new HashSet<>();
+        testSet.add(15);
+        testSet.add(-6);
+        testSet.add(10);
+        testSet.add(0);
+
+        // Act
+        int difference = Practice.largestMinusSmallest(testSet);
+
+        // Assert
+        // Should be 15
+        assertEquals(21, difference);
+    }
 }
 
 
