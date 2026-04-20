382e2c96e59e272e09679e247142513751a5565a
Buay Dak <buaypd@gmail.com>
2026-04-19 23:00:09 -0700

    Used hashset for the largest/smallest problem

--- src/Practice.java
+++ src/Practice.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Practice {
     /**
      * Returns the difference between the largest and smallest integer in an array.
@@ -16,4 +19,29 @@ public class Practice {
     // For each method you are only required to implement it for one of the data
     // structures. But use a different data structure for each method. For example,
     // do maxDiff with an array, the next question with a Set, etc.
+
+
+
+
+
+    // Find the difference between the largest and smallest integer in HashSet
+    // o(n)
+    public static int largestMinusSmallest(Set<Integer> nums){
+        int smallest = Integer.MIN_VALUE;
+        int largest = Integer.MAX_VALUE;;
+    // Set<Integer> nums = new HashSet<>();
+    for (int num : nums) {
+        if (num > largest) {
+            largest = num;
+            }
+        }
+        for (int num : nums) {
+            if (num < smallest) {
+                smallest = num;
+            }
+        }
+
+      return largest - smallest; 
+    
+    }
 }
